936318cde9fed925aaef75d83d35142b06d42a80
lsh0704 <1365236295@qq.com>
2018-07-03 10:18:48 +0800

    在自己原有的代码基础上增加对楼宇管理员、学生信息、楼宇信息、宿舍信息和学生出勤情况的增加、删除、查找和修改功能的操作

---
+++ src/com/db/DBHelper.java
@@ -0,0 +1,32 @@
+package com.db;
+
+import java.sql.*;
+
+public class DBHelper {	
+	private String dbUrl="jdbc:mysql://localhost:3306/sushe?characterEncoding=utf8";
+	private String dbUser="root";
+	private String dbPassword="";
+	private String jdbcName="com.mysql.jdbc.Driver";
+	
+	//�������ݿ�
+	public Connection getConn(){
+		Connection conn = null;
+		try{
+			Class.forName(jdbcName);
+		}
+		catch(Exception e){}
+		try{
+			conn=DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+		}
+		catch(SQLException ex){}
+		return conn;		
+	}
+	
+//    ����
+	public static void main(String[] args)
+	{
+		System.out.println(new DBHelper().getConn());
+		
+	}
+	
+}
