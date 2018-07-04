d88764819cd4fc356c6aec4fd351c17d7b25db76
0704 <layaipipi@gmail.com>
2018-07-04 11:38:52 +0800

    登陆校验

---
+++ src/com/action/LoginValidation.java
@@ -0,0 +1,21 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginValidation extends ActionSupport {
+	private String Username;
+	private String Password;
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	
+}
